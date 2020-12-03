18cd183faf57ff88bce261fca23f652d08c5cabc
Mario <marketingkg@mail.ru>
2020-12-03 14:21:42 +0600

    showtheform is working

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-class  App{
-    constructor(){
-     this.handlerEventsListeners()
-     this.$form = document.getElementById('form')
+class App {
+  constructor() {
+    this.eventListenerHandler();
+    this.$form = document.getElementById("form");
+    this.$noteTitle = document.getElementById("note-title");
+    this.$noteText = document.getElementById("note-text");
+    this.$formButtons = document.getElementById("form-buttons");
+  }
+  eventListenerHandler() {
+    document.body.addEventListener("click", (e) => {
+      this.formWasClickedHandler(e);
+    });
+  }
+  formWasClickedHandler(e) {
+    const theFormWasClicked = this.$form.contains(e.target);
+    if (theFormWasClicked) {
+      this.openForm();
     }
-    handlerEventsListeners(){
-          document.body.addEventListener('click',e =>{
-                 this.handlerForm(e)
-          })
-    }
-    handlerForm(e){
-        const theFormWasClicked = this.$form.contains(e.target)
-        if(theFormWasClicked){
-            console.log(`this was clicked`)
-        }
-    }
+  }
+  openForm() {
+    console.log(`rhis is called`);
+    this.$noteTitle.style.display = "block";
+    this.$noteText.style.display = "block";
+    this.$formButtons.style.display = "block";
+  }
 }
-new App()
+new App();
