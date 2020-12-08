8c67bda50082b36043ae41917c35d71a5310550f
Mario Arriola <64659499+ARRIOLALEO@users.noreply.github.com>
2020-12-08 18:21:21 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,147 @@
-// first we define our class
 class App {
-  //  contructor is called when we call our class with the word bew
-  //  the constructor inicilate our object
   constructor() {
-    //  we set our form and we bind it with the this
-    this.$form = document.getElementById("form");
-    this.$addNote = document.getElementById("submit-button");
-    //we set the elements of our form in order to show or hide them
-    this.$noteTittle = document.getElementById("note-title");
-    this.$noteText = document.getElementById("note-text");
-    this.$formBtns = document.getElementById("form-buttons");
+    this.notes = [];
+    this.title = '';
+    this.text = '';
+    this.id = '';
 
-    // on creation we load ours listeners
-    this.setAllLsitenerOnTheWebpage();
+    this.$placeholder = document.querySelector("#placeholder");
+    this.$form = document.querySelector("#form");
+    this.$notes = document.querySelector("#notes");
+    this.$noteTitle = document.querySelector("#note-title");
+    this.$noteText = document.querySelector("#note-text");
+    this.$formButtons = document.querySelector("#form-buttons");
+    this.$formCloseButton = document.querySelector("#form-close-button");
+    this.$modal = document.querySelector(".modal");
+    this.$modalTitle = document.querySelector(".modal-title");
+    this.$modalText = document.querySelector(".modal-text");
+    this.$modalCloseButton = document.querySelector('.modal-close-button');
+
+    this.addEventListeners();
   }
-  // we need to set all listeners in our website we create a method
-  setAllLsitenerOnTheWebpage() {
-    //   we add a click function to our body "e" is the element that was clicked
-    document.body.addEventListener("click", (e) => {
-      // we call and set the form handler
-      this.formClickHandler(e);
-      this.addNoteHandler(e);
+
+  addEventListeners() {
+    document.body.addEventListener("click", event => {
+      this.handleFormClick(event);
+      this.selectNote(event);
+      this.openModal(event);
     });
+
+    this.$form.addEventListener("submit", event => {
+      event.preventDefault();
+      const title = this.$noteTitle.value;
+      const text = this.$noteText.value;
+      const hasNote = title || text;
+      if (hasNote) {
+        // add note
+        this.addNote({ title, text });
+      }
+    });
+
+    this.$formCloseButton.addEventListener("click", event => {
+      event.stopPropagation();
+      this.closeForm();
+    });
+    
+    this.$modalCloseButton.addEventListener('click', event => {
+      this.closeModal(event);  
+    })
   }
-  // we create th method that will handal the form we need to see if was cliked we add the event
-  formClickHandler(e) {
-    // we checked if the form was clicked
-    const theFormWasClicked = this.$form.contains(e.target);
-    // if the form was cliked we show the form in order to add our note to the syste
-    if (theFormWasClicked) {
+
+  handleFormClick(event) {
+    const isFormClicked = this.$form.contains(event.target);
+
+    const title = this.$noteTitle.value;
+    const text = this.$noteText.value;
+    const hasNote = title || text;
+
+    if (isFormClicked) {
       this.openForm();
+    } else if (hasNote) {
+      this.addNote({ title, text });
     } else {
-      // if was clicked somewhere else and the form is open the form must be closed
       this.closeForm();
     }
   }
-  // this method handle the add the note
-  addNoteHandler(e) {
-    const addTheNoteWasClicked = this.$addNote.contains(e.target);
-    //  if it was cliked
-    if (addTheNoteWasClicked) {
-      alert(this.$noteTittle.value);
-    }
-  }
-  // this function open the form in our system
+
   openForm() {
-    this.$noteText.style.display = "block";
-    this.$noteTittle.style.display = "block";
-    this.$formBtns.style.display = "block";
+    this.$form.classList.add("form-open");
+    this.$noteTitle.style.display = "block";
+    this.$formButtons.style.display = "block";
   }
-  // this function close our form it is just inverse to open
+
   closeForm() {
-    this.$noteTittle.style.display = "none";
-    this.$formBtns.style.display = "none";
+    this.$form.classList.remove("form-open");
+    this.$noteTitle.style.display = "none";
+    this.$formButtons.style.display = "none";
+    this.$noteTitle.value = "";
+    this.$noteText.value = "";
+  }
+  
+  openModal(event) {
+     if (event.target.closest('.note')) {
+        this.$modal.classList.toggle('open-modal');  
+        this.$modalTitle.value = this.title;
+        this.$modalText.value = this.text;
+     }
+  }
+  
+  closeModal(event) {
+     this.editNote(); 
+     this.$modal.classList.toggle('open-modal');
+  }
+
+  addNote({ title, text }) {
+    const newNote = {
+      title,
+      text,
+      color: "white",
+      id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
+    };
+    this.notes = [...this.notes, newNote];
+    this.displayNotes();
+    this.closeForm();
+  }
+  
+  editNote() {
+     const title = this.$modalTitle.value;
+     const text = this.$modalText.value;
+     this.notes = this.notes.map(note => 
+       note.id === Number(this.id) ? { ...note, title, text } : note
+     );
+     this.displayNotes();
+  }
+  
+  selectNote(event) {
+     const $selectedNote = event.target.closest('.note');
+     if (!$selectedNote) return;
+     const [$noteTitle, $noteText] = $selectedNote.children;
+     this.title = $noteTitle.innerText;
+     this.text = $noteText.innerText;
+     this.id = $selectedNote.dataset.id;
+  }
+
+  displayNotes() {
+    const hasNotes = this.notes.length > 0;
+    this.$placeholder.style.display = hasNotes ? "none" : "flex";
+
+    this.$notes.innerHTML = this.notes
+      .map(
+        note => `
+        <div style="background: ${note.color};" class="note" data-id="${note.id}">
+          <div class="${note.title && "note-title"}">${note.title}</div>
+          <div class="note-text">${note.text}</div>
+          <div class="toolbar-container">
+            <div class="toolbar">
+              <img class="toolbar-color" src="https://icon.now.sh/palette">
+              <img class="toolbar-delete" src="https://icon.now.sh/delete">
+            </div>
+          </div>
+        </div>
+     `
+      )
+      .join("");
   }
 }
-// we call our app to be active in our page we just have one page we just call our App
+
 new App();
